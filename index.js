05fdc83b5de1d1e72fcf4f38afaa6ac19227dce4
13Anisha <119130741+13Anisha@users.noreply.github.com>
2024-08-18 10:19:54 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app=express();
+const PORT=5000;
+const API_URL="https://api.jikan.moe/v4";
+const NUM_ANIMES=18;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+app.get("/",async (req,res)=>{
+    try{
+        //generating an array of Promises
+        const AnimePromises=Array.from({length:NUM_ANIMES},()=>{
+             return axios.get(`${API_URL}/random/anime?sfw=true`);
+        })
+        
+        //wait for the promises to get resolved
+        const results=await Promise.all(AnimePromises);
+        
+        const animes = results.map(result => result.data.data).filter(anime => !anime.explicit_genres.length);
+
+        // console.log(animes[1]);
+        res.render("index.ejs",{content:animes});
+
+    }
+    catch(error){
+        console.log(`Error:${error.message}`);
+    }
+    
+    
+});
+
+app.post("/search",async (req,res)=>{
+    try{
+        
+        const result=await axios.get(`${API_URL}/anime?q=${req.body.anime}&sfw=true&`);
+        console.log(result.data);
+        if(result.data.data.length==0)
+        {
+            res.render("search.ejs");
+        }
+        else{
+            res.render("search.ejs",{content:result.data.data});
+
+        }
+        
+
+
+       
+    }
+
+    catch(error){
+        console.log(error.message);
+    }
+})
+
+app.post("/anime",async (req,res)=>{
+    try{
+       
+        const animeData=await axios(`${API_URL}/anime/${req.body.animeID}`);
+        const animeInfo=animeData.data.data;
+        
+        res.render("anime.ejs",{Info:animeInfo});
+        console.log(animeInfo);
+
+
+    }
+    catch(error){
+        console.log(error.message);
+    }
+   
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
